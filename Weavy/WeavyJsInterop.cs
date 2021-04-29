5ff50e4daed581f6b54aa33255061ba5329329fd
Klas Jersevi <klas@weavy.com>
2021-04-29 22:52:24 +0200

    Initial weavy working

---
+++ Weavy/WeavyJsInterop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorApp.Weavy {
+    public class WeavyJsInterop : IDisposable {
+        private readonly IJSRuntime js;
+        private IJSObjectReference weavyBridge;
+        private object defaultOptions;
+
+        public WeavyJsInterop(IJSRuntime js) {
+            this.js = js;
+            defaultOptions = new { jwt = "{insert jwt here}" };
+        }
+
+        async public Task Init() {
+            weavyBridge = await js.InvokeAsync<IJSObjectReference>("import", "./weavyJsInterop.js");
+        }
+
+        async public ValueTask<IJSObjectReference> Weavy(object options = null) {
+            return await weavyBridge.InvokeAsync<IJSObjectReference>("weavy", new object[] { defaultOptions, options });
+        }
+    
+        async public ValueTask<IJSObjectReference> Space(IJSObjectReference weavy, object spaceSelector = null) {
+            return await weavy.InvokeAsync<IJSObjectReference>("space", new object[] { spaceSelector });
+        }
+
+        async public ValueTask<IJSObjectReference> App(IJSObjectReference space, object appSelector = null) {
+            return await space.InvokeAsync<IJSObjectReference>("app", new object[] { appSelector });
+        }
+
+        public void Dispose() {
+            weavyBridge?.DisposeAsync();
+        }
+    }
+}
+
